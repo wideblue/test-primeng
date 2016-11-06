3b96506a6faf05401b33716580ce1730924af696
Damjan Dvorsek <big.wide.blue@gmail.com>
2016-11-06 22:14:51 +0100

    primeng with angular-cli webpack

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {SelectItem} from 'primeng/primeng';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  cities: SelectItem[];
+
+    selectedCity: string;
+
+    constructor() {
+        this.cities = [];
+        this.cities.push({label:'Select City', value:null});
+        this.cities.push({label:'New York', value:{id:1, name: 'New York', code: 'NY'}});
+        this.cities.push({label:'Rome', value:{id:2, name: 'Rome', code: 'RM'}});
+        this.cities.push({label:'London', value:{id:3, name: 'London', code: 'LDN'}});
+        this.cities.push({label:'Istanbul', value:{id:4, name: 'Istanbul', code: 'IST'}});
+        this.cities.push({label:'Paris', value:{id:5, name: 'Paris', code: 'PRS'}});
+    }
 }
